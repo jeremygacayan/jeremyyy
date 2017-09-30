18ce6088559e13621469d18312fd2fd05647a4d3
jeremygacayan <31788550+jeremygacayan@users.noreply.github.com>
2017-09-30 15:03:42 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Basic Login using PHP and MySql</title>
+	<link rel="stylesheet" href="style.css" type="text/css" />
+
+</head>
+<body>
+
+<body background="background.jpg">
+
+<form action="index.html" class="login">
+    <h1>Facebook</h1>
+    <input type="email" name="email" class="login-input" placeholder="Email Address" autofocus>
+    <input type="password" name="password" class="login-input" placeholder="Password">
+    <input type="submit" value="Login" class="login-submit">
+    <p class="login-help"><a href="index.html">Forgot password?</a></p>
+
+
+	  <div class="signupform">
+      <form id="signup" action="" method="post">
+        <p>
+          <input type="text" id="firstname" name="firstname" placeholder="First Name" value="" class="radius mini" />
+          <input type="text" id="lastname" name="lastname" placeholder="Last Name" value="" class="radius mini" />
+        </p>
+        <p>
+          <input type="text" id="email" name="email" placeholder="Your Email" value="" class="radius" />
+        </p>
+        <p>
+          <input type="text" id="remail" name="remail" placeholder="Re-enter Email" class="radius" />
+        </p>
+        <p>
+          <input type="password" id="password" name="password" placeholder="New Password" class="radius" />
+        </p>
+        <p>
+          <button class="radius title id="signup"  name="signup">Sign Up for Facebook</button>
+        </p>
+		
+		<li>
+<select><option>Day</option></select>
+<select><option>Month</option></select>
+<select><option>Year</option></select>
+</li>
+<li><input type="radio">Female <input type="radio">Male</li>
+<
+<li><input type="submit" value="Create an account"></li>
+      </form>
+    </div>
+	
+	
+	
+	
+	
+	
+
+</body>
+</html>
+
